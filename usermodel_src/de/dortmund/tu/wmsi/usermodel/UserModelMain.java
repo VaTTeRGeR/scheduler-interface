20288cac9ec6f8c396169bba086438f630455d12
Florian Schmickmann <florian.schmickmann@tu-dortmund.de>
2016-09-16 11:21:24 +0200

    test de le test

--- usermodel_src/de/dortmund/tu/wmsi/usermodel/UserModelMain.java
+++ usermodel_src/de/dortmund/tu/wmsi/usermodel/UserModelMain.java
@@ -7,14 +7,14 @@ public class UserModelMain {
 
 	public static void main(String[] args) {
 		SimulationInterface si = SimulationInterface.instance();
-		for (int i = 0; i < 1; i++) {
+		for (int i = 0; i < 10; i++) {
 			si.configure("usermodel_config/simulation_easy_avg.properties");
 			si.simulate();
 		}
 		
 		AVGWTLogger.resetLog();
 
-		for (int i = 0; i < 1; i++) {
+		for (int i = 0; i < 10; i++) {
 			si.configure("usermodel_config/simulation_overtime_easy_absolute.properties");
 			si.simulate();
 		}
