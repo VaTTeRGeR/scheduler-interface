d74f637f8e9373277347a4c467f623dbcfd0078d
Florian Schmickmann <flo.schmickmann@googlemail.com>
2016-10-20 11:48:08 +0200

    ...

--- usermodel_src/de/dortmund/tu/wmsi/usermodel/UserModelMain.java
+++ usermodel_src/de/dortmund/tu/wmsi/usermodel/UserModelMain.java
@@ -9,37 +9,41 @@ public class UserModelMain {
 
 	private static final boolean doAVG = false;
 	private static final boolean doSWF = true;
+	private static final int numSimAVG = 20;
+	private static final int numSimSWF = 5;
 	
 	public static void main(String[] args) {
 		if(doAVG) {
-			runsim("mira_bpf_config/simulation_batch_priority_fair_estimate_avg.properties", "mira_bpf_config/simulation_easy_avg.properties", "mira_estimate_avg");
-			runsim("lanl_bpf_config/simulation_batch_priority_fair_estimate_avg.properties", "lanl_bpf_config/simulation_easy_avg.properties", "lanl_estimate_avg");
-			runsim("ctc_bpf_config/simulation_batch_priority_fair_estimate_avg.properties", "ctc_bpf_config/simulation_easy_avg.properties", "ctc_estimate_avg");
-			runsim("kth_bpf_config/simulation_batch_priority_fair_estimate_avg.properties", "kth_bpf_config/simulation_easy_avg.properties", "kth_estimate_avg");
+			runsim(numSimAVG,"mira_bpf_config/simulation_batch_priority_fair_estimate_avg.properties", "mira_bpf_config/simulation_easy_avg.properties", "mira_estimate_avg");
+			runsim(numSimAVG,"lanl_bpf_config/simulation_batch_priority_fair_estimate_avg.properties", "lanl_bpf_config/simulation_easy_avg.properties", "lanl_estimate_avg");
+			runsim(numSimAVG,"ctc_bpf_config/simulation_batch_priority_fair_estimate_avg.properties", "ctc_bpf_config/simulation_easy_avg.properties", "ctc_estimate_avg");
+			runsim(numSimAVG,"kth_bpf_config/simulation_batch_priority_fair_estimate_avg.properties", "kth_bpf_config/simulation_easy_avg.properties", "kth_estimate_avg");
 		}
 		if(doSWF) {
-			runsim("mira_bpf_config/simulation_batch_priority_fair_estimate_swf.properties", "mira_bpf_config/simulation_easy_swf.properties", "mira_estimate_swf");
-			runsim("lanl_bpf_config/simulation_batch_priority_fair_estimate_swf.properties", "lanl_bpf_config/simulation_easy_swf.properties", "lanl_estimate_swf");
-			runsim("ctc_bpf_config/simulation_batch_priority_fair_estimate_swf.properties", "ctc_bpf_config/simulation_easy_swf.properties", "ctc_estimate_swf");
-			runsim("kth_bpf_config/simulation_batch_priority_fair_estimate_swf.properties", "kth_bpf_config/simulation_easy_swf.properties", "kth_estimate_swf");
+			for (int i = 1; i <= numSimSWF; i++) {
+				runsim(1,"mira_bpf_config/simulation_batch_priority_fair_estimate_swf.properties", "mira_bpf_config/simulation_easy_swf.properties", "mira_estimate_swf"+i);
+				runsim(1,"lanl_bpf_config/simulation_batch_priority_fair_estimate_swf.properties", "lanl_bpf_config/simulation_easy_swf.properties", "lanl_estimate_swf"+i);
+				runsim(1,"ctc_bpf_config/simulation_batch_priority_fair_estimate_swf.properties", "ctc_bpf_config/simulation_easy_swf.properties", "ctc_estimate_swf"+i);
+				runsim(1,"kth_bpf_config/simulation_batch_priority_fair_estimate_swf.properties", "kth_bpf_config/simulation_easy_swf.properties", "kth_estimate_swf"+i);
+			}
 		}
 	}
 	
-	private static void runsim(String config0, String config1, String appendix) {
+	private static void runsim(int n, String config0, String config1, String appendix) {
 		//StatisticalMathHelper.setUserAccepteableWaitTimeParameters(c1, c2);
 
 		SimulationInterface si = SimulationInterface.instance();
 
 		AVGWTLogger.resetLog();
 
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i < n; i++) {
 			si.configure(config0);
 			si.simulate();
 		}
 
 		AVGWTLogger.resetLog();
 
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i < n; i++) {
 			si.configure(config1);
 			si.simulate();
 		}
