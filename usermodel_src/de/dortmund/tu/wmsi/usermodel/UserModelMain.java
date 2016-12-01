73f3a94a3ed52b8a2c686db996b1ed8353e7da87
Florian Schmickmann <florian.schmickmann@tu-dortmund.de>
2016-12-01 13:27:36 +0100

    ...

--- usermodel_src/de/dortmund/tu/wmsi/usermodel/UserModelMain.java
+++ usermodel_src/de/dortmund/tu/wmsi/usermodel/UserModelMain.java
@@ -4,8 +4,6 @@ import java.io.File;
 
 import de.dortmund.tu.wmsi.SimulationInterface;
 import de.dortmund.tu.wmsi.usermodel.model.BatchCreator;
-import de.dortmund.tu.wmsi.usermodel.model.userestimate.EstimateSampler;
-import de.dortmund.tu.wmsi.usermodel.model.userestimate.ProgressiveEstimateSampler;
 import de.dortmund.tu.wmsi.usermodel.util.StatisticalMathHelper;
 import de.dortmund.tu.wmsi_swf_example.logger.AVGWTLogger;
 
@@ -22,48 +20,31 @@ public class UserModelMain {
 		}
 		
 		if(doAVG) {
-			runsim(numSimAVG, "mira_all_avg",
-					"mira_bpf_config/simulation_batch_priority_fair_estimate_avg.properties",
-					//"mira_bpf_config/simulation_batch_priority_fair_estimate_uq_avg.properties",
-					"mira_bpf_config/simulation_batch_priority_fair_estimate_bfo_avg.properties",
-					//"mira_bpf_config/simulation_batch_priority_fair_bfo_avg.properties",
-					//"mira_bpf_config/simulation_batch_priority_fair_avg.properties",
-					"mira_bpf_config/simulation_easy_estimate_avg.properties"//,
-					//"mira_bpf_config/simulation_easy_avg.properties",
-					//"mira_bpf_config/simulation_overtime_easy_absolute.properties"
+			/*runsim(numSimAVG, "hpc2n_avg",
+					"hpc2n_bpf_config/simulation_batch_priority_fair_estimate_bfo_avg.properties",
+					"hpc2n_bpf_config/simulation_easy_es_avg.properties",
+					"hpc2n_bpf_config/simulation_overtime_easy_absolute.properties"
 					);
-//			
-//			runsim(numSimAVG, "lanl_avg",
-//					"lanl_bpf_config/simulation_batch_priority_fair_estimate_avg.properties",
-//					"lanl_bpf_config/simulation_batch_priority_fair_estimate_bfo_avg.properties",
-//					"lanl_bpf_config/simulation_batch_priority_fair_avg.properties",
-//					"lanl_bpf_config/simulation_easy_estimate_avg.properties",
-//					"lanl_bpf_config/simulation_easy_avg.properties", "lanl_bpf_estimate_avg");
-//			
-//			runsim(numSimAVG, "ctc_avg",
-//					"ctc_bpf_config/simulation_batch_priority_fair_estimate_avg.properties",
-//					"ctc_bpf_config/simulation_batch_priority_fair_estimate_bfo_avg.properties",
-//					"ctc_bpf_config/simulation_batch_priority_fair_avg.properties",
-//					"ctc_bpf_config/simulation_easy_estimate_avg.properties",
-//					"ctc_bpf_config/simulation_easy_avg.properties");
-//			
-			//runsim(numSimAVG, "ctc_es_progressive",
-					//"ctc_bpf_config/simulation_batch_priority_fair_estimate_avg.properties",
-					//"ctc_bpf_config/simulation_batch_priority_fair_estimate_bfo_avg.properties",
-					//"ctc_bpf_config/simulation_batch_priority_fair_avg.properties",
-					//"ctc_es_config/simulation_easy_es_avg.properties",
-					//"ctc_es_config/simulation_easy_es_swf_avg.properties"//,
-					//"ctc_es_config/simulation_easy_estimate_avg.properties",
-					//"ctc_es_config/simulation_easy_avg.properties"
-					//);
 			
-//			runsim(numSimAVG, "kth_avg",
-//					"kth_bpf_config/simulation_batch_priority_fair_estimate_avg.properties",
-//					"kth_bpf_config/simulation_batch_priority_fair_estimate_bfo_avg.properties",
-//					"kth_bpf_config/simulation_batch_priority_fair_avg.properties",
-//					"kth_bpf_config/simulation_easy_estimate_avg.properties",
-//					"kth_bpf_config/simulation_easy_avg.properties");
+			runsim(numSimAVG, "lanl_avg",
+					"lanl_bpf_config/simulation_batch_priority_fair_estimate_bfo_avg.properties",
+					"lanl_bpf_config/simulation_easy_estimate_avg.properties",
+					"lanl_bpf_config/simulation_overtime_easy_absolute.properties"
+					);*/
+			
+			runsim(numSimAVG, "ctc_avg",
+					"ctc_bpf_config/simulation_batch_priority_fair_estimate_bfo_avg.properties",
+					"ctc_bpf_config/simulation_easy_estimate_avg.properties",
+					"ctc_bpf_config/simulation_overtime_easy_absolute.properties"
+					);
+					
+			runsim(numSimAVG, "kth_avg",
+					"kth_bpf_config/simulation_batch_priority_fair_estimate_bfo_avg.properties",
+					"kth_bpf_config/simulation_easy_estimate_avg.properties",
+					"kth_bpf_config/simulation_overtime_easy_absolute.properties"
+					);
 		}
+		
 		if(doSWF) {
 			for (int i = 1; i <= numSimSWF; i++) {
 				runsim(1, "mira_bpf_estimate_swf"+i, "mira_bpf_config/simulation_batch_priority_fair_estimate_swf.properties", "mira_bpf_config/simulation_easy_swf.properties");
@@ -88,8 +69,8 @@ public class UserModelMain {
 				si.configure(configs[i]);
 				si.simulate();
 
-				EstimateSampler.saveEstimateMatrix();
-				ProgressiveEstimateSampler.saveEstimateMatrix();
+				//EstimateSampler.saveEstimateMatrix();
+				//ProgressiveEstimateSampler.saveEstimateMatrix();
 			}
 			
 			
@@ -98,6 +79,10 @@ public class UserModelMain {
 		}
 
 		try {
+			if(new File("avg_output_"+appendix).exists()) {
+				new File("avg_output_"+appendix).delete();
+			}
+				
 			new File("avg_output/").renameTo(new File("avg_output_"+appendix));
 			new File("avg_output/").mkdir();
 		} catch (Exception e) {
